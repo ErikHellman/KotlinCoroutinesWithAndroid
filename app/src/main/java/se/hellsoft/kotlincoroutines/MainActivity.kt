7953c57d084f6cd6b1ee87ff1b40edb00ce5ecb2
Erik Hellman <erik.hellman@hellsoft.se>
2017-12-14 20:56:45 +0100

    Producer/Consumer

--- app/src/main/java/se/hellsoft/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/se/hellsoft/kotlincoroutines/MainActivity.kt
@@ -1,41 +1,104 @@
 package se.hellsoft.kotlincoroutines
 
+import android.database.Cursor
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.channels.ReceiveChannel
+import kotlinx.coroutines.experimental.channels.produce
+import kotlinx.coroutines.experimental.launch
+import java.util.concurrent.atomic.AtomicInteger
 
 class MainActivity : AppCompatActivity() {
   data class Cat(val id: Int, val uri: String, val photo: Bitmap)
 
+  fun loge(e: Throwable, message: () -> String) {
+    Log.e("MainActivity", message(), e)
+  }
+
+  fun logd(message: () -> String) {
+    Log.d("MainActivity", message())
+  }
+
+  private var channel: ReceiveChannel<Cat>? = null
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    val linearLayoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+    linearLayoutManager.stackFromEnd = true
+    listOfCats.layoutManager = linearLayoutManager
     val catAdapter = CatAdapter()
-    catAdapter.cats = loadCats()
+    catAdapter.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+      override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+        super.onItemRangeInserted(positionStart, itemCount)
+        listOfCats.scrollToPosition(positionStart)
+      }
+    })
     listOfCats.adapter = catAdapter
+    logd { "Load photos!" }
+    val number = AtomicInteger(0)
+    channel = produce {
+      val queryDatabase = queryDatabase()
+      val cursor = object : Cursor by queryDatabase {
+        override fun close() {
+          logd { "Closing!" }
+          queryDatabase.close()
+        }
+      }
+
+      cursor.use {
+        while (it.moveToNext()) {
+          send(cursorToData(it))
+          logd { "Cursor is at last: ${it.isLast}" }
+        }
+      }
+    }
+
+    floatingActionButton.setOnClickListener {
+      async(UI) {
+        val cat = channel?.receiveOrNull()
+        cat?.let {
+          logd { "Adding next cat on ${Thread.currentThread().name}: $cat" }
+          catAdapter.cats += it
+          catAdapter.notifyItemInserted(catAdapter.cats.lastIndex)
+        }
+      }
+    }
   }
 
-  private fun loadCats(): List<Cat> {
+  override fun onDestroy() {
+    super.onDestroy()
+    channel?.let {
+      logd { "Cancelling channel!" }
+      it.cancel()
+    }
+  }
+
+  private fun queryDatabase(): Cursor {
     val projection = arrayOf(MediaStore.Images.Media._ID, MediaStore.Images.Media.TITLE)
-    val cursor = MediaStore.Images.Media.query(contentResolver, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection)
-    return cursor?.let {
-      generateSequence { if(it.moveToNext()) it else null }
-          .map {
-            val photoId = it.getInt(it.getColumnIndex(MediaStore.Images.Media._ID))
-            val uri = Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, photoId.toString())
-            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
-            Cat(photoId, it.getString(it.getColumnIndex(MediaStore.Images.Media.TITLE)), bitmap)
-          }
-          .toList()
-    } ?: emptyList()
+    return MediaStore.Images.Media.query(contentResolver, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection)
+  }
+
+  private fun cursorToData(it: Cursor): Cat {
+    logd { "Building a cat on ${Thread.currentThread().name}" }
+    val photoId = it.getInt(it.getColumnIndex(MediaStore.Images.Media._ID))
+    val uri = Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, photoId.toString())
+    val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+    val scaled = Bitmap.createScaledBitmap(bitmap, bitmap.width / 3, bitmap.height / 3, true)
+    return Cat(photoId, it.getString(it.getColumnIndex(MediaStore.Images.Media.TITLE)), scaled)
   }
 
   class CatAdapter : RecyclerView.Adapter<CatViewHolder>() {
@@ -52,9 +115,10 @@ class MainActivity : AppCompatActivity() {
       return CatViewHolder(view)
     }
 
+
   }
 
   class CatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-   val photoView = itemView.findViewById<ImageView>(R.id.catPhoto)
+    val photoView = itemView.findViewById<ImageView>(R.id.catPhoto)
   }
 }
